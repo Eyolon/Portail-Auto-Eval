bb1e1bd3182ec2dc148b9e789c8027ddcc571639
Eyolon <valgame@live.fr>
2016-04-18 16:36:35 +0200

    Récupération des notes fonctionnelles, manque plus qu'a les stocker en base !

--- client/app/evaluation/EvaluationController.js
+++ client/app/evaluation/EvaluationController.js
@@ -3,7 +3,8 @@ function EvaluationController(EvaluationService, ipCookie) {
 	this.isSuccess = true;
     this.listQuestionnaire = [];
 	this.questionnaire;
-	this.user = ipCookie('utilisateur'); 
+	this.user = ipCookie('utilisateur');
+	this.Answers = {};
 	
 	function onSuccess() {
         self.isSuccess = true;
@@ -12,11 +13,18 @@ function EvaluationController(EvaluationService, ipCookie) {
 	function onError() {
 		self.isSuccess = false;
 	}
+	
+	this.repondre = function repondre() {
+        console.log(self.Answers);
+        //objet qui conteint l'id de la question et la réponse.
+        //il faut faire passer l'id utilisateur et la remarque sera le path du drive pour la preuve 
+    };
     
     this.getListQuestionnaires = function getListQuestionnaires(){
     	//self.listQuestionnaire = EvaluationService.listFormulaireFull.post({idService: '1'}, onSuccess, onError);
-        
-    	self.listQuestionnaire = EvaluationService.listFormulaireFull.post({idService: self.user.service['id']}, onSuccess, onError);
+        self.listQuestionnaire = EvaluationService.listFormulaireFull.post({idService: self.user.service['id']}, onSuccess, onError);
+
+
     };
 	
 	/*Plus un cas de test plutot qu'utile*/
